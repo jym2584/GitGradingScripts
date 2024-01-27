ed7e378843923e3d6c0ff1e707f0b74b49580cf9
Jin Moon <jym2584@rit.edu>
2024-01-26 22:34:11 -0500

    Added function to prune local repositories

--- repo_utils.py
+++ repo_utils.py
@@ -1,7 +1,13 @@
 from git import Repo
 import yaml
 from threading_utils import synchronized
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+import os
+import platform
 import pprint
+import shutil
+import subprocess
 pp = pprint.PrettyPrinter(indent=4)
 
 SUBMISSION_LOGS = "config/submissions.yml" # logs submissions since last pull
@@ -135,6 +141,120 @@ def save_submissions(submissions_dict: dict):
     with open(SUBMISSION_LOGS, 'w') as file:
         yaml.dump(submissions_dict, file)
 
+
+def parse_duration_string(delta_string:str):
+    """Parses the duration string to a format that is datetime compatible
+
+    Args:
+        delta_string (str): Time in string
+            d - day
+            h - hour
+                    
+            i.e. 
+            - 90d = 90 days
+            - 12h = 12 hours
+            - 90d12h = 90 days and 12 hours
+        
+
+    Returns:
+        relativedelta: time interval that is datetime compatible (i.e. 90days 12hours)
+    """
+    duration = relativedelta()
+    
+    current_value = '' # parsing integer values
+    for char in delta_string:
+        if char.isdigit(): current_value += char
+        else:
+            unit = char.lower()
+            if current_value:
+                value = int(current_value)
+                if unit == 'd': # day
+                    duration = duration + relativedelta(days=value)
+                elif unit == 'h': # hour
+                    duration = duration + relativedelta(hours=value)
+                elif unit == 'm': # minute
+                    duration = duration + relativedelta(minutes=value)
+                # elif unit == 's': # seconds
+                #     duration = duration + relativedelta(seconds=value)
+                current_value = ''
+    return duration
+
+def get_file_creation_time(file_path):
+    if platform.system() == 'Windows': creation_time = os.path.getctime(file_path)
+    else:
+        # Use stat to get the creation time on linux systems
+        stat_info = os.stat(file_path)
+        try: creation_time = stat_info.st_birthtime
+        except AttributeError: creation_time = stat_info.st_mtime # use st_mtime as a fallback
+
+    return datetime.fromtimestamp(creation_time)
+
+def prune_local_repos(clone_output_path:str, delta_string:str, prompt_removal:bool):
+    """Removes local repositories that are cloned by the script
+
+    Args:
+        clone_output_path (str): Output path to search and delete files
+        remove_before_timestamp (datetime): Remove folders older than this timestamp
+        prompt_removal (bool): Determines whether a prompt is required to remove files from the local system
+    """
+    
+    dirs_to_remove = dict()
+    searched_paths = set()
+    
+    timestamp_now = datetime.now()
+    remove_before_timestamp = timestamp_now - parse_duration_string(delta_string)
+    # grab files that are older than the timestamp
+    for dir_parent in os.listdir(clone_output_path):
+        directory_path = f"{clone_output_path}/{dir_parent}/"
+        for directory_name in os.listdir(directory_path):
+            full_path = f"{directory_path}{directory_name}/"
+            path_timestamp = get_file_creation_time(full_path)
+            
+            if remove_before_timestamp > path_timestamp: # remove everything before path_timestamp 
+                dirs_to_remove[directory_name] = {'created_timestamp': path_timestamp, 'full_path': full_path}
+                searched_paths.add(directory_path)
+    
+    if len(dirs_to_remove) == 0:
+        return
+    
+    print("--------------------DIRECTORIES TO DELETE----------------------")
+    print("Searched paths:")
+    for path in searched_paths: print(f"- {path}")
+    print()
+    print(f"Current timestamp         :  {timestamp_now}")
+    print(f"Deleting files older than :  {remove_before_timestamp}")
+    print(f"Difference                :  {delta_string}")
+    print("---------------------------------------------------------------")
+    for dir in dirs_to_remove:
+        print(f"- {dir} (created: {dirs_to_remove[dir]['created_timestamp']})")
+    print()
+    if prompt_removal:
+        prompt = input("Do you want to delete these files? (`yes` for yes; any other input for no): ")
+        if not prompt:
+            print("Not deleting the above files.")
+            return
+    
+    print("---------------------DELETING DIRECTORIES----------------------")
+    for dir in dirs_to_remove:
+        path = dirs_to_remove[dir]['full_path']
+        try: 
+            shutil.rmtree(path)
+        except Exception as e: 
+            print(f"(!) Cannot delete {path}\n{e}\n")
+            
+    print("Done!")
+
+
+def prune_old_submissions_logs(delta_string:str):
+    pass
+    
+
+
+if __name__ == "__main__":
+    pass
+
+    prune_local_repos("E:/GitHub/GithubClassroomScripts/assignments", "0m", True)
+        
 # organization_name = "My Test Org"
 # assignment_name = "Test-assignment"
 
